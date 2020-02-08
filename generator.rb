318c2fa31ae50f7196f31f0ba5040d200c8e726d
Ravinder Deol <ravinder.r.deol@gmail.com>
2020-02-08 15:59:03 +0000

    Working. Generates Course Titles.

---
+++ generator.rb
@@ -0,0 +1,38 @@
+class CourseTitleGenerator
+
+  def initialize(topic, qty)
+    @topic = topic.capitalize
+    @qty = qty.to_i
+  end
+
+  def create_titles
+    titles = all_titles.sample(@qty)
+    titles.each do |title|
+      puts title.gsub! '{topic}', @topic
+    end
+  end
+
+  def all_titles
+    [
+      'The Complete 2020 {topic} Course',
+      '{topic}: From Beginner To Expert',
+      'The Ultimate {topic} Course',
+      '{topic} In-Depth: Become A Complete Expert',
+      'The {topic} Bootcamp',
+      '100 {topic} Exercises: Evaluate & Improve Your Skills',
+      '{topic} - The Practical Guide',
+      'The Essential Guide To {topic}',
+      '{topic} For Beginners - Master {topic} Quickly',
+      'Complete {topic} Bootcamp: Go From Zero To Hero'
+    ]
+  end
+end
+
+puts 'Enter Your Topic'
+topic = gets.chomp
+
+puts 'How Many Titles'
+qty = gets.chomp
+
+generator = CourseTitleGenerator.new(topic, qty)
+generator.create_titles
